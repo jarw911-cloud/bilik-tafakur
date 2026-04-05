9d10f972e17d4ce5bae84a47fabf7d3288937626
Jarw <mac@macs-MacBook-Pro.local>
2026-04-05 20:44:40 +0700

    Miwiti Bilik Tafakur

---
+++ tailwind.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class', // Ini yang akan mengaktifkan tombol siang/malam kita
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}

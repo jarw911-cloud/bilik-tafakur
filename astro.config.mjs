a210c8e86924aad1f2f8bbbb42f0a9d6717d042d
Jarw911-cloud <jarw911@gmail.com>
2026-04-06 18:21:30 +0700

    Force rebuild for sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap'; // 1. TAMBAH IKI
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-  // 2. PESHTEKAKE ALAMAT IKI BENER (Penting nggo sitemap)
+  // PESHTEKAKE IKI PERSIS ALAMAT VERCEL-MU
   site: 'https://bilik-tafakur.vercel.app', 
-  integrations: [
-    tailwind(), 
-    sitemap() // 3. TAMBAH IKI
-  ],
+  integrations: [tailwind(), sitemap()],
 });

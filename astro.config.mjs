7207f6ed5b5933d99d77ba5f22d7bb2160661cb4
Jarw911-cloud <jarw911@gmail.com>
2026-04-06 18:01:44 +0700

    Manual install sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap'; // 1. TAMBAH IKI
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  // 2. PESHTEKAKE ALAMAT IKI BENER (Penting nggo sitemap)
+  site: 'https://bilik-tafakur.vercel.app', 
+  integrations: [
+    tailwind(), 
+    sitemap() // 3. TAMBAH IKI
+  ],
 });

78e0fcd0b9e98883e545b03e357389e341ef9c03
Jarw911-cloud <jarw911@gmail.com>
2026-04-30 10:31:13 +0700

    mmm

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap'; // 1. Tambahake import niki
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://bilik-tafakur.my.id', // 2. Wajib isi domain asli web-e
+  // Niki wajib wonten kagem sitemap!
+  site: 'https://bilik-tafakur.my.id',
+  
   integrations: [
     tailwind(),
-    sitemap() // 3. Lebetake sitemap() teng mriki
+    sitemap() // Plugin sitemap mlebet mriki
   ]
 });
